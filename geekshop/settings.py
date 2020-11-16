9f5c8e08690290c3fdf829c0664b811958d77a98
Fedor Polyakov <pismo.na.pochtu@gmail.com>
2020-11-16 20:03:14 +0300

    try change postgresql user x3

--- geekshop/settings.py
+++ geekshop/settings.py
@@ -123,13 +123,13 @@ DATABASES = {
     #     'NAME': BASE_DIR / 'db.sqlite3',
     # }
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'geekshop',
         #'USER': 'postgres',
         'USER': 'django',
         'PASSWORD': 'geekbrains',
-        'HOST': '127.0.0.1',
-        'PORT': '5432',
+        # 'HOST': '127.0.0.1',
+        # 'PORT': '5432',
     }
 }
 
